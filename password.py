497448dbd8ad1c75250ee5b40600e346731bb35c
nehaalpine <36499168+nehaalpine@users.noreply.github.com>
2018-02-21 12:01:07 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,15 @@
+string.ascii_letters
+Concatenation of the ascii (upper and lowercase) letters
+
+string.digits
+The string '0123456789'. 	
+
+string.punctuation
+String of ASCII characters which are considered punctuation characters in the C 
+locale.
+
+print string.ascii_letters
+
+print string.digits
+
+print string.punctuation
